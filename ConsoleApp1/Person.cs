4beb2577512cef430c0cfddad37fddbfb8cc3dc0
Viktoria Myronova <ipz212_mvl@student.ztu.edu.ua>
2022-02-11 23:58:35 +0200

    Added Method to Person Class

--- ConsoleApp1/Person.cs
+++ ConsoleApp1/Person.cs
@@ -8,5 +8,20 @@ namespace ConsoleApp1
 {
     class Person
     {
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+
+        public Person(string lastName, string firstName, DateTime dateOfBirth)
+        {
+            LastName = lastName;
+            FirstName = firstName;
+            DateOfBirth = dateOfBirth;
+        }
+        public int GetAge()
+        {
+            TimeSpan age = DateTime.Now - DateOfBirth;
+            return (int)age.TotalDays / 365;
+        }
     }
 }
