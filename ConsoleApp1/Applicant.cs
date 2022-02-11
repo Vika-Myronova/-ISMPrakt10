ad87bac6dcca5f2551c795d21763908c6b03f7e2
Viktoria Myronova <ipz212_mvl@student.ztu.edu.ua>
2022-02-12 00:04:19 +0200

    Added Applicant Class

---
+++ ConsoleApp1/Applicant.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Applicant : Person
+    {
+        public double NumberOfPointOfEEPCertificates { get; set; }
+        public double NumberOfPointsForEducationDocument { get; set; }
+        public string SecondarySchoolName { get; set; }
+
+        public Applicant(string lastName, string firstName, DateTime dateOfBirth, double numberOfPointOfEEPCertificates, double numberOfPointsForDocumentOnEducation, string secondarySchoolName) : base(lastName, firstName, dateOfBirth)
+        {
+            NumberOfPointOfEEPCertificates = numberOfPointOfEEPCertificates;
+            NumberOfPointsForEducationDocument = numberOfPointsForDocumentOnEducation;
+            SecondarySchoolName = secondarySchoolName;
+        }
+    }
+}
