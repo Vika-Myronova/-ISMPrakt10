e6abc28a3ec8691a3e2c453b78ab63b4fe500c16
Viktoria Myronova <ipz212_mvl@student.ztu.edu.ua>
2022-02-12 14:06:07 +0200

    Added Lecturer Class with Constructor

---
+++ ConsoleApp1/LibraryUser.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class LibraryUser : Person
+    {
+        public long ReadersTicketNumber { get; set; }
+        public DateTime DateOfIssue { get; set; }
+        public double MonthlyReaderShipFee { get; set; }
+        public LibraryUser(string lastName, string firstName, DateTime dateOfBirth, long readersTicketNumber, DateTime dateOfIssue, double monthlyReaderShipFee) : base(lastName, firstName, dateOfBirth)
+        {
+            ReadersTicketNumber = readersTicketNumber;
+            DateOfIssue = dateOfIssue;
+            MonthlyReaderShipFee = monthlyReaderShipFee;
+        }
+    }
+
+}
