ad8e75b176f75aee0cef806e05be38d9fff76173
Viktoria Myronova <ipz212_mvl@student.ztu.edu.ua>
2022-02-12 13:46:57 +0200

    Adeed Lecturer Class with Constructor

---
+++ ConsoleApp1/Lecturer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Lecturer : Person
+    {
+        public string Position { get; set; }
+        public string Department { get; set; }
+        public string UniversityName { get; set; }
+
+        public Lecturer(string lastName, string firstName, DateTime dateOfBirth, string position, string department, string universityName) : base(lastName, firstName, dateOfBirth)
+        {
+            Position = position;
+            Department = department;
+            UniversityName = universityName;
+        }
+    }
+}
