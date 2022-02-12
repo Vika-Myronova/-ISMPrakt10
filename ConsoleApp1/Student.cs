509597a8992cf2ee608a04eb38b0596abd5ac13f
Viktoria Myronova <ipz212_mvl@student.ztu.edu.ua>
2022-02-12 13:40:29 +0200

    Added GetStudentsGroupNameAndCourse Method to Student Class

--- ConsoleApp1/Student.cs
+++ ConsoleApp1/Student.cs
@@ -6,7 +6,23 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp1
 {
-    class Student
+    class Student : Person
     {
+        public string CourseOfStudy { get; set; }
+        public string GroupName { get; set; }
+        public string Faculty { get; set; }
+        public string UniversityName { get; set; }
+        public Student(string lastName, string firstName, DateTime dateOfBirth, string courseOfStudy, string groupName, string faculty, string universityName) : base(lastName, firstName, dateOfBirth)
+        {
+            CourseOfStudy = courseOfStudy;
+            GroupName = groupName;
+            Faculty = faculty;
+            UniversityName = universityName;
+        }
+        public string GetStudentsGroupNameAndCourse()
+        {
+            string groupAndCourse = "Група " + GroupName + " " + CourseOfStudy;
+            return groupAndCourse;
+        }
     }
 }
